b1935e7c82a3729d93821edb3040fbc6320f1231
Jindiwei <dwjinfd@gmail.com>
2017-06-16 23:47:17 +0800

    first commit

---
+++ src/main/java/com/fduss/OoapApplication.java
@@ -0,0 +1,12 @@
+package com.fduss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OoapApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fduss/OoapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fduss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OoapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
